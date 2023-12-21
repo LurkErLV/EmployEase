f2ea2d002e8e4a285766a11822ffbb57e2140671
Alberts Matrozis <62105039+LurkErLV@users.noreply.github.com>
2023-12-21 06:22:24 +0200

    feat: Implemented types for next-auth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth, { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            id: string,
+            email: string,
+            name: string | null | undefined,
+            surname: string | null | undefined,
+            role: string,
+            applies: string,
+        } & DefaultSession["user"]
+    }
+}
